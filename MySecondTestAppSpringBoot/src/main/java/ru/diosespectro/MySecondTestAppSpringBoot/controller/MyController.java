3c14c77c8f564e1ed9fd34c69fec6017f30ba3bf
DiosEspectro <diosespectro@gmail.com>
2023-10-05 23:44:35 +0500

    Курс 2. Лабораторная работа №3.

--- MySecondTestAppSpringBoot/src/main/java/ru/diosespectro/MySecondTestAppSpringBoot/controller/MyController.java
+++ MySecondTestAppSpringBoot/src/main/java/ru/diosespectro/MySecondTestAppSpringBoot/controller/MyController.java
@@ -1,6 +1,8 @@
 package ru.diosespectro.MySecondTestAppSpringBoot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -9,55 +11,71 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.diosespectro.MySecondTestAppSpringBoot.exception.UnsupportedCodeException;
 import ru.diosespectro.MySecondTestAppSpringBoot.exception.ValidationFailedException;
-import ru.diosespectro.MySecondTestAppSpringBoot.model.Request;
-import ru.diosespectro.MySecondTestAppSpringBoot.model.Response;
+import ru.diosespectro.MySecondTestAppSpringBoot.model.*;
+import ru.diosespectro.MySecondTestAppSpringBoot.service.ModifyResponseService;
 import ru.diosespectro.MySecondTestAppSpringBoot.service.ValidationService;
+import ru.diosespectro.MySecondTestAppSpringBoot.util.DateTimeUtil;
 
 import javax.validation.Valid;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Slf4j
 @RestController
 public class MyController {
 
     private final ValidationService validationService;
+    private final ModifyResponseService modifyResponseService;
 
     @Autowired
-    public MyController(ValidationService validationService) {
+    public MyController(ValidationService validationService, @Qualifier("ModifySystemTimeResponseService") ModifyResponseService modifyResponseService) {
         this.validationService = validationService;
+        this.modifyResponseService = modifyResponseService;
     }
 
     @PostMapping(value = "/feedback")
     public ResponseEntity<Response> feedback(@Valid @RequestBody Request request, BindingResult bindingResult) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        log.info("request: {}", request);
 
         Response response = Response.builder()
                 .uid(request.getUid())
                 .operationUid(request.getOperationUid())
-                .systemTime(simpleDateFormat.format(new Date()))
-                .code("success")
-                .errorCode("")
-                .errorMessage("")
+                .systemTime(DateTimeUtil.getCustomFormat().format(new Date()))
+                .code(Codes.SUCCESS)
+                .errorCode(ErrorCodes.EMPTY)
+                .errorMessage(ErrorMessages.EMPTY)
                 .build();
+
+        log.info("response: {}", response);
 
         try {
             validationService.isValid(bindingResult);
         } catch (ValidationFailedException e) {
-            response.setCode("failed");
-            response.setErrorCode("ValidationException");
-            response.setErrorMessage("Ошибка валидации");
+            response.setCode(Codes.FAILED);
+            response.setErrorCode(ErrorCodes.VALIDATION_EXCEPTION);
+            response.setErrorMessage(ErrorMessages.VALIDATION);
+
+            log.error("error response: {} {}", response, bindingResult.getFieldError().getDefaultMessage());
+
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         } catch (UnsupportedCodeException e) {
-            response.setCode("failed");
-            response.setErrorCode("UnsupportedCodeException");
-            response.setErrorMessage("Не поддерживаемая ошибка");
+            response.setCode(Codes.FAILED);
+            response.setErrorCode(ErrorCodes.UNSUPPORTED_EXCEPTION);
+            response.setErrorMessage(ErrorMessages.UNSUPPORTED);
+
+            log.error("error response: {} {}", response, bindingResult.getFieldError().getDefaultMessage());
+
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            response.setCode("failed");
-            response.setErrorCode("UnknownException");
-            response.setErrorMessage("Произошла непредвиденная ошибка");
+            response.setCode(Codes.FAILED);
+            response.setErrorCode(ErrorCodes.UNKNOWN_EXCEPTION);
+            response.setErrorMessage(ErrorMessages.UNKNOWN);
+
+            log.error("response: {} {}", response, bindingResult.getFieldError().getDefaultMessage());
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+        modifyResponseService.modify(response);
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(modifyResponseService.modify(response), HttpStatus.OK);
     }
 }
--- MySecondTestAppSpringBoot/src/main/java/ru/diosespectro/MySecondTestAppSpringBoot/model/Request.java
+++ MySecondTestAppSpringBoot/src/main/java/ru/diosespectro/MySecondTestAppSpringBoot/model/Request.java
@@ -15,24 +15,40 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 public class Request {
 
-    @NotBlank
+    @NotBlank(message = "Поле uid не заполнено")
     private String uid;
 
-    @NotBlank
+    @NotBlank(message = "Поле operationUid не заполнено")
     private String operationUid;
 
-    private String systemName;
 
-    @NotBlank
+    private Systems systemName;
+
+    @NotBlank(message = "Поле systemTime не заполнено")
     private String systemTime;
 
     private String source;
 
-    @Min(1)
-    @Max(100000)
+    @Min(value = 1, message = "Значение communicationId не должно быть меньше 1")
+    @Max(value = 100000, message = "Значение communicationId не должно быть больше 100000")
     private int communicationId;
 
     private int templateId;
     private int productCode;
     private int smsCode;
+
+    @Override
+    public String toString() {
+        return "{" +
+                "uid='" + uid + '\'' +
+                ", operationUid='" + operationUid + '\'' +
+                ", systemName='" + systemName + '\'' +
+                ", systemTime='" + systemTime + '\'' +
+                ", source='" + source + '\'' +
+                ", communicationId=" + communicationId +
+                ", templateId=" + templateId +
+                ", productCode=" + productCode +
+                ", smsCode=" + smsCode +
+                '}';
+    }
 }
